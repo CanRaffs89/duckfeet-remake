a9ba93c948fa8f449c7299063d82a1d9324f1bd5
Can <24660563+CanRaffs89@users.noreply.github.com>
2020-01-18 21:55:00 +0000

    Added JS slider to Featured section

---
+++ index.js
@@ -0,0 +1,50 @@
+// Featured Section Slider
+let slides = document.querySelectorAll('.slide'),
+    arrowRight = document.querySelector('#right-arrow'),
+    arrowLeft = document.querySelector('#left-arrow'),
+    current = 0;
+
+// Clear Slides    
+function reset(){
+    for(let i =0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+}
+
+// Initialise Featured Slider
+function startSlides(){
+    reset();
+    slides[0].style.display = 'grid';
+}
+
+// Show previous section
+function slideLeft() {
+    reset();
+    slides[current - 1].style.display = 'grid';
+    current--;
+}
+
+// Show next section 
+function slideRight() {
+    reset();
+    slides[current + 1].style.display = 'grid';
+    current++;
+}
+
+// Left Arrow event listener 
+arrowLeft.addEventListener('click', function(){
+    if(current === 0){
+        current = slides.length;
+    }
+    slideLeft();
+});
+
+// Right Arrow event listener
+arrowRight.addEventListener('click', function(){
+    if(current === slides.length - 1){
+        current = -1
+    }
+    slideRight();
+});
+
+startSlides();
